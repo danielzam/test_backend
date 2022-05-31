3ceabb788c0ce22066e10a6536d1acf7a1afc0fd
Daniel Zamora <danielzam.c@gmail.com>
2022-05-31 06:23:35 -0500

    Faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         User::factory(1000)->create();
+        $this->call([
+            DocumentTypeSeeder::class,
+        ]);
+        User::factory(1000)->create();
     }
 }
